08da1c4a9bf05db0d06e80e88b67c8e9edcf285b
akashgamage <bakashgamage@gmail.com>
2024-01-01 23:14:40 +0530

    Updates

--- To Do List/Other/To_Do_ListApp.swift
+++ To Do List/Other/To_Do_ListApp.swift
@@ -15,7 +15,7 @@ struct To_Do_ListApp: App {
     }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- To Do List/Views/MainView.swift
+++ To Do List/Views/MainView.swift
@@ -13,7 +13,7 @@ struct MainView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello,!")
+            Text("Hello, World!")
         }
         .padding()
     }
