fe099af8b09b281a394bd515ff424378606d0803
akashgamage <bakashgamage@gmail.com>
2024-01-01 19:08:53 +0530

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Akash Gamage on 2024-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
